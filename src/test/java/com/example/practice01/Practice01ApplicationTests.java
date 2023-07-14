a02621fe031c334a043a05f4ea33038da52f7d8b
ParkJunSeong <tjd782323@gmail.com>
2023-07-14 16:14:14 +0900

    Initial commit

---
+++ src/test/java/com/example/practice01/Practice01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
